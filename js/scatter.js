85e81ab97fc62045ea244cfa5ba8d66b6f6412c9
Daniel Chen <danielchenphhs@gmail.com>
2017-10-14 21:24:02 -0500

    color scale and legend

--- js/scatter.js
+++ js/scatter.js
@@ -20,9 +20,9 @@
 
   // Type colors from http://www.epidemicjohto.com/t882-type-colors-hex-colors
   this.colors = ['#A8A77A','#EE8130','#6390F0','#F7D02C','#7AC74C',
-                 '#96D9D6','#C22E28','#A33EA1','#E2BF65','#A98FF3',
-                 '#F95587','#A6B91A','#B6A136','#735797','#6F35FC',
-                 '#705746','#B7B7CE','#D685AD'];
+  '#96D9D6','#C22E28','#A33EA1','#E2BF65','#A98FF3',
+  '#F95587','#A6B91A','#B6A136','#735797','#6F35FC',
+  '#705746','#B7B7CE','#D685AD'];
   
 
   this.initVis();
@@ -86,8 +86,8 @@ Scatter.prototype.createVis = function() {
   vis.x = d3.scaleLinear().range([0, vis.width]);
   vis.y = d3.scaleLinear().range([vis.height, 0]);
   vis.c = d3.scaleOrdinal().range(vis.colors)
-        .domain(["Fire", "Water", "Electric","Grass","Ice","Fighting","Poison","Ground",
-          "Flying","Psychic","Bug","Rock","Ghost","Dragon","Dark","Steel","Fairy"]);
+  .domain(["Fire", "Water", "Electric","Grass","Ice","Fighting","Poison","Ground",
+    "Flying","Psychic","Bug","Rock","Ghost","Dragon","Dark","Steel","Fairy"]);
 
   vis.x.domain([0, d3.max(vis.fin_data, function(d) { 
     return d[vis.x_stat]; 
@@ -104,21 +104,25 @@ Scatter.prototype.createVis = function() {
   vis.yAxis = vis.svg.append("g")
   .call(d3.axisLeft(vis.y));
 
-  vis.xLabel = vis.svg.selectAll(".x-label")
+  vis.xLabel = vis.svg.selectAll(".xLabel")
   .append("text")
-  .attr("class", "x-label")
-  .text(vis.x_stat)
-  .attr("transform", function(d,i) {
-    return "translate(" + (vis.width / 2) + "," + vis.height + ")";
-  });
+  .attr("class", "xLabel")
+  .attr("x", vis.width)
+  .attr("y", -6)
+  .style("text-anchor", "end")
+  .text(vis.x_stat);
 
-  vis.yLabel = vis.svg.selectAll(".y-label")
+  vis.yLabel = vis.svg.selectAll(".yLabel")
   .append("text")
-  .attr("class", "y-label")
-  .text(vis.y_stat)
-  .attr("transform", function(d,i) {
-    return "translate(-10," + (vis.height / 2) + ")rotate(270)";
-  });
+  .attr("class", "yLabel")
+  .attr("transform", "rotate(-90)")
+  .attr("y", 6)
+  .attr("dy", ".71em")
+  .style("text-anchor", "end")
+  // .attr("transform", function(d,i) {
+  //   return "translate(-10," + (vis.height / 2) + ")rotate(270)";
+  // })
+  .text(vis.y_stat);
 
   vis.dots = vis.svg.selectAll(".dots")
   .data(vis.fin_data)
@@ -129,19 +133,19 @@ Scatter.prototype.createVis = function() {
   .attr("cy", function(d) { return vis.y(d[vis.y_stat]); })
   .attr("stroke-opacity", .7)
   .attr("fill-opacity", .2)
-  // .attr("stroke", function(d) {
-  //   return vis.getColor(d.type1);
-  // })
+    // .attr("stroke", function(d) {
+    //   return vis.getColor(d.type1);
+    // })
   .attr("stroke", function(d) { return vis.c(d.type1); })
   .attr("fill", function(d) {
     if (d.type2) {
       return vis.c(d.type2);
-      // return vis.getColor(d.type2);
-    } else {
-      return vis.c(d.type1);
-      // return vis.getColor(d.type1);
-    }
-  })
+        // return vis.getColor(d.type2);
+      } else {
+        return vis.c(d.type1);
+        // return vis.getColor(d.type1);
+      }
+    })
   .attr("stroke-width", 3)
   .on("mouseover", function(d) {
     vis.tip.show(d);
@@ -150,21 +154,19 @@ Scatter.prototype.createVis = function() {
     vis.tip.hide(d);
   });
 
-  vis.updateVis();
-
   // vis.legend = svg.selectAll(".legend")
   // .data()
 
   vis.legend = vis.svg.selectAll(".legend")
-      .data(vis.c.domain())
-    .enter().append("g")
-      .attr("class", "legend")
-      .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+  .data(vis.c.domain())
+  .enter().append("g")
+  .attr("class", "legend")
+  .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
 
   // draw legend colored rectangles
   vis.legend.append("rect")
   // .attr("class", "legend-rect")
-  .attr("x", width - 18)
+  .attr("x", vis.width - 18)
   .attr("width", 18)
   .attr("height", 18)
   .attr("stroke-opacity", .6)
@@ -176,20 +178,15 @@ Scatter.prototype.createVis = function() {
 
   // draw legend text
   vis.legend.append("text")
-    .attr("x", vis.width - 24)
-      .attr("y", 9)
-      .attr("dy", ".35em")
-    .style("text-anchor", "end")
-    .text(function(d) { return d;})
+  .attr("x", vis.width - 24)
+  .attr("y", 9)
+  .attr("dy", ".35em")
+  .style("text-anchor", "end")
+  .text(function(d) { return d;})
 
-  vis.legend.exit().remove();
+  // vis.legend.exit().remove();
 
-  // draw legend colored rectangles
-  legend.append("rect")
-      .attr("x", width - 18)
-      .attr("width", 18)
-      .attr("height", 18)
-      .style("fill", color);
+  vis.updateVis();
 };
 
 Scatter.prototype.updateVis = function() {
